d09b26f2a799a6343176be731cc5f64bcee1d7ae
Piotr-Trzcinski <cinekenic@go2.pl>
2021-05-17 16:45:21 +0200

    get the article id

--- js/script.js
+++ js/script.js
@@ -59,7 +59,11 @@ function generateTitleLinks() {
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = "";
   /* for each article */
-  /* get the article id */
+  const articles = document.querySelectorAll(optArticleSelector);
+  for (let article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+  }
   /* find the title element */
   /* get the title from the title element */
   /* create HTML of the link */
