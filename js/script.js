ab36607ef184d242eca1e5fbd5c55caa707c7c71
Piotr-Trzcinski <cinekenic@go2.pl>
2021-05-17 15:14:19 +0200

    remove class active

--- js/script.js
+++ js/script.js
@@ -10,10 +10,20 @@ const titleClickHandler = function (event) {
   console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll(".titles a.active");
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll("article.active");
+  console.log(activeArticles);
+  for (let activeArticl of activeArticles) {
+    activeArticl.classList.remove("active");
+  }
 
   /* get 'href' attribute from the clicked link */
 
