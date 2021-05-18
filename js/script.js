ff34e24634d291d7c975ae0c9041d959130f09e3
Piotr-Trzcinski <cinekenic@go2.pl>
2021-05-18 11:19:59 +0200

    change articleId to articleTitle in linkHTML

--- js/script.js
+++ js/script.js
@@ -59,10 +59,10 @@ function generateTitleLinks() {
     /* get the article id */
     const articleId = article.getAttribute("id");
     /* find the title element */
-    const articleTitle = article.querySelector(optTitleSelector);
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
     /* get the title from the title element */
     /* create HTML of the link */
-    const linkHTML = `<li><a href="#${articleId}"><span>${articleId}</span></a></li>`;
+    const linkHTML = `<li><a href="#${articleTitle}"><span>${articleTitle}</span></a></li>`;
     /* insert link into titleList */
     html = html + linkHTML;
   }
